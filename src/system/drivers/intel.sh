9918eab738c01f6bd43e99f2b0ac43f5d1d69b33
Mike <80540554+beachmichael55@users.noreply.github.com>
2025-09-28 19:28:18 -0400

    Add files via upload

---
+++ src/system/drivers/intel.sh
@@ -0,0 +1,23 @@
+source src/cmd.sh
+
+function intel_drivers() {
+    local -r inlst="
+        mesa
+        lib32-mesa
+        vulkan-intel
+        lib32-vulkan-intel
+        vulkan-icd-loader
+        lib32-vulkan-icd-loader
+        intel-media-driver
+        intel-gmmlib
+        onevpl-intel-gpu
+        vulkan-mesa-layers
+        libva-mesa-driver
+        lib32-libva-mesa-driver
+        mesa-vdpau
+        lib32-mesa-vdpau
+		intel-gpu-tools
+    "
+
+    install_lst "${inlst}"
+}
