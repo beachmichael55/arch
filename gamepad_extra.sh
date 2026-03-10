3826d08be2014e79c0c49f7e41b7a78174857fb8
Mike <80540554+beachmichael55@users.noreply.github.com>
2026-03-10 19:14:27 -0400

    Delete gamepad_extra.sh

--- gamepad_extra.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if command -v yay &> /dev/null; then
-        AUR=yay
-elif command -v paru &> /dev/null; then
-        AUR=paru
-fi
-$AUR -S --noconfirm joystickwake xpadneo-dkms-git xone-dkms-git xone-dongle-firmware dualsensectl-git
